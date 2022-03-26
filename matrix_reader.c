e64c23eea64132ca694b4812a08e968c3bb3b057
andreacarotti <andrea1.carotti@mail.polimi.it>
2022-03-26 13:13:53 +0100

    added CSR

--- matrix_reader.c
+++ matrix_reader.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
 #define ARRAY_LENGTH 100
 #define MAX_FLOAT_VALUE 10
 
@@ -8,6 +10,8 @@
 
 
 void spmv_coo();
+void spmv_csr();
+
 float *generate_x();
 void print_info();
 void print_y();
@@ -17,7 +21,7 @@ void print_debug();
 int main(int argc, char *argv[]){
 	int number_of_nz = 0;
 
-	if (argc != 3){
+	if (argc != 4){
 		printf("Error: you need to provide R (rows) and C (columns) of the matrix to read\n");
 		exit(0);
 	}
@@ -38,17 +42,23 @@ int main(int argc, char *argv[]){
 	float data[ARRAY_LENGTH];
 	*/
 
-	
 	//MALLOC ARRAY DEFINITION
 	int *row;
 	int *indices;
+	int *ptr;
 	float *data;
+
+	//CSR AND COO
 	
-	row = (int*) malloc(ARRAY_LENGTH * sizeof(int));
 	indices = (int*) malloc(ARRAY_LENGTH * sizeof(int));
 	data = (float*) malloc(ARRAY_LENGTH * sizeof(float));
-	
 
+	//COO
+	row = (int*) malloc(ARRAY_LENGTH * sizeof(int));
+
+	//CSR
+	ptr = (int*) malloc(ARRAY_LENGTH * sizeof(int));
+	
 	if (row == NULL || indices == NULL || data == NULL) {
         printf("Memory not allocated.\n");
         exit(0);
@@ -58,12 +68,18 @@ int main(int argc, char *argv[]){
 	int data_iterator = 0;
 	int row_iterator = 0;
 	int indices_iterator = 0;
+	int ptr_iterator = 0;
 	int i,j;
+
+	ptr[0] = 0;
+	ptr_iterator = 1;
+
 
 	for (i = 0; i < rows; i++){
 		for (j = 0; j < cols; j++){
 			
 			fscanf(p,"%f",&num_read);
+
 
 			if (num_read != 0){
 				data[data_iterator] = num_read;
@@ -76,43 +92,40 @@ int main(int argc, char *argv[]){
 				indices_iterator++;
 
 				number_of_nz++;
-			}			
+			}
+
+
 		}
+
+		//adding the current number_of_nz to the ptr array
+		ptr[ptr_iterator] = number_of_nz;
+		ptr_iterator++;
 	}
+
+	
 
 
 
 	float *x = generate_x(cols);
 
 	//print_info(rows, cols, number_of_nz);
-
-	//allocating y
 	float *y = (float*) malloc(cols * sizeof(float));
 
-	//print_debug(row, indices, data, number_of_nz, x, y, cols);
+	if(strcmp(argv[3],"COO")==0){
+		spmv_coo(row, indices, data, number_of_nz, x, y);
+	}
+	else if (strcmp(argv[3],"CSR")==0){
+		spmv_csr(ptr, indices, data, number_of_nz, x, y);
+	}
 
-	spmv_coo(row, indices, data, number_of_nz, x, y);
+	//print_debug(row, indices, data, number_of_nz, x, y, cols);
 
 	print_y(y, cols);
 
 	return 0;
 }
 
-void print_debug(int *row, int *indices, float *data, int number_of_nz, float *x, float *y, int cols){
-	printf("\n");
-	for (int i = 0; i < number_of_nz; i++){
-		printf("row[%d] = %d\n", i, row[i]);
-		printf("indices[%d] = %d\n", i, indices[i]);
-		printf("data[%d] = %f\n", i, data[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < cols; i++){
-		printf("x[%d] = %f\n", i, x[i]);
-		printf("y[%d] = %f\n", i, y[i]);
-	}
-	printf("\n");
 
-}
 
 
 float *generate_x(int C){
@@ -129,20 +142,35 @@ float *generate_x(int C){
 	return array;
 }
 
-void print_info(rows, cols, number_of_nz){
-	float rows_float = rows;
-	float cols_float = cols;
-	float number_of_nz_float = number_of_nz;
-	printf("Number of NZ: %d\n", number_of_nz);
-	printf("Density: %f\n", number_of_nz_float/(rows_float*cols_float));
-}
-
 void spmv_coo(int *rowind, int *colind, float *val, int number_of_nz, float *x, float *y) {
   int i;
   for(i = 0; i < number_of_nz ; i++){
     y[rowind[i]] += val[i] * x[colind[i]];
     //printf("y[%d] = %f\n", i, y[i]);
   }
+}
+
+
+void spmv_csr(int *row_ptr, int *colind, float *val, int number_of_nz, float *x, float *y)
+{
+  int i, j;
+  float temp;
+  for(i = 0; i < number_of_nz ; i++)
+  {
+    temp = y[i];
+    for(j = row_ptr[i]; j < row_ptr[i+1]; j++){
+      temp += val[j] * x[colind[j]];
+    }
+    y[i] = temp;
+  }
+}
+
+void print_info(rows, cols, number_of_nz){
+	float rows_float = rows;
+	float cols_float = cols;
+	float number_of_nz_float = number_of_nz;
+	printf("Number of NZ: %d\n", number_of_nz);
+	printf("Density: %f\n", number_of_nz_float/(rows_float*cols_float));
 }
 
 void print_y(float *y, int length){
@@ -153,4 +181,32 @@ void print_y(float *y, int length){
 
 
 
-
+/*
+void print_debug(int *row, int *indices, float *data, int number_of_nz, float *x, float *y, int cols, int *ptr){
+	printf("\n");
+	for (int i = 0; i < number_of_nz; i++){
+		printf("row[%d] = %d\n", i, row[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < number_of_nz; i++){
+		printf("indices[%d] = %d\n", i, indices[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < number_of_nz; i++){
+		printf("data[%d] = %f\n", i, data[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < number_of_nz; i++){
+		printf("ptr[%d] = %d\n", i, ptr[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < cols; i++){
+		printf("x[%d] = %f\n", i, x[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < cols; i++){
+		printf("y[%d] = %f\n", i, y[i]);
+	}
+	printf("\n");
+}
+*/
